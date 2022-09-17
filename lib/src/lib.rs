53cefdc34c649af1d7b438bd3f7d76b7d0afda47
JimJ92120 <57893611+JimJ92120@users.noreply.github.com>
2022-09-17 23:02:05 +0700

    feat(board): add board struct and cell enum

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,27 +1,57 @@
-use wasm_bindgen::prelude::*;
-use web_sys::console;
 
-// #[cfg(feature = "wee_alloc")]
-// #[global_allocator]
-// static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+use wasm_bindgen::prelude::*;
+// use js_sys::*;
 
 #[wasm_bindgen]
 extern "C" {
     #[wasm_bindgen(js_namespace = console)]
-    fn log(s: &str);
+    pub fn log(s: &str);
 }
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    log(&format!("Hello, {}!", name));
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+    DEAD = 0,
+    ALIVE = 1,
 }
 
-#[wasm_bindgen(start)]
-pub fn main_js() -> Result<(), JsValue> {
-    #[cfg(debug_assertions)]
-    console_error_panic_hook::set_once();
+#[wasm_bindgen]
+#[derive(Debug)]
+pub struct Board {
+    height: usize,
+    width: usize,
+    cells: Vec<Cell>
+}
 
-    console::log_1(&JsValue::from_str("Hello world from Rust!"));
+#[wasm_bindgen]
+impl Board {
+    #[wasm_bindgen(constructor)]
+    pub fn new(height: usize, width: usize) -> Self {
+        let mut board = Self {
+            height,
+            width,
+            cells: vec![]
+        };
 
-    Ok(())
+        board.set_cells();
+
+        board
+    }
+
+    pub fn get_board_length(&self) -> usize {
+        self.height * self.width
+    }
+
+    pub fn set_cells(&mut self) {
+        self.cells = (0..self.get_board_length()).map(|index| Cell::DEAD).collect();
+    }
+
+    pub fn get_cells(&self) -> js_sys::Uint8Array {
+        let arr: Vec<u8> = self.cells.iter().map(|i| {
+            *i as u8
+        }).collect();
+
+        js_sys::Uint8Array::from(&arr[..])
+    }
 }
