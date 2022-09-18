fad62ef960dc8948de60767c1c9b6666bd5f7eea
JimJ92120 <57893611+JimJ92120@users.noreply.github.com>
2022-09-18 22:03:24 +0700

    feat(app): add engine and create animation frame to update and draw board cells

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,25 @@
-import init, * as wasm from "../dist/lib";
+import init, {
+  Board
+} from "../dist/lib";
+import Engine from "./engine";
 
-init()
-  .then(() => {
-    wasm.greet("WASM");
+window.addEventListener("DOMContentLoaded", () => {
+  init()
+    .then(() => {
+      const engine: Engine = new Engine("scene");
+      const board: Board = new Board(engine.resolution[1], engine.resolution[0]);
+      const vertexSize: number = 1;
+      
+      let loop = 0.0;
+      const animate: FrameRequestCallback = () => {
+        if (loop % 100 === 0) {
+          board.toggle_cells();
+          engine.draw(new Float32Array(board.get_cells()), vertexSize);
+        }
+    
+        loop = requestAnimationFrame(animate);
+      };
+    
+      requestAnimationFrame(animate);
   });
+});
